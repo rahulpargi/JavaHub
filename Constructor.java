b775ffd305f2f3c434aae2c21e7cb208c23d1963
Rahul Pargi <rahulpargi@gmail.com>
2018-05-26 14:22:18 -0500

    Constructor

--- Constructor.java
+++ Constructor.java
@@ -13,14 +13,22 @@ class Geek{
 		this.num=num;
 		System.out.println("Constructor with parameters");
 	}
+	//Constructor Overloading
+	Geek(String name){
+		this.name=name;
+		System.out.println("Constructor with single arguments"+" "+name);
+	}
 
 }
+//There are no return statements in constructor but it return current class instance
 class Constructor{
 	public static void main(String[] args){
 		Geek geek1=new Geek();
 		Geek geek2=new Geek("adam",1);
+		Geek geek3=new Geek("Hello");
 		System.out.println(geek2.name);
 		System.out.println(geek2.num);
+		System.out.println(geek3.name);
 	}
 
 }
