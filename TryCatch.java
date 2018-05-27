4ed37d0da5ce71d96b57168251cbce419b7e61fb
Rahul Pargi <rahulpargi@gmail.com>
2018-05-26 19:56:23 -0500

    TryCatchFinally

--- TryCatch.java
+++ TryCatch.java
@@ -4,10 +4,13 @@ class TryCatch{
 	public static void main(String[] args){
 		int[] arr=new int[4];
 		try{
-			int i=arr[4];
+			int i=arr[3];
 			System.out.println("Inside try block");
-		}catch(ArrayIndexOutOfBoundsException e){
+		}
+		/*catch(NullPointerException e){
 			System.out.println("Exception caught");
+		}*/finally{
+			System.out.println("finally block executed");
 		}
 		System.out.println("Outside try catch clause");
 	}
