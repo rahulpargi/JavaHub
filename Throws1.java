7c1cb33acd1afc7416dbc3d8c10aedd0210e4f32
Rahul Pargi <rahulpargi@gmail.com>
2018-05-26 21:28:02 -0500

    Throws

---
+++ Throws1.java
@@ -0,0 +1,14 @@
+class Throws1{
+	static void fun() throws IllegalAccessException{
+		System.out.println("Inside fun()");throw new IllegalAccessException("demo");
+	}
+	public static void main(String[] args) throws InterruptedException{
+		try{
+			fun();
+		}
+		catch(IllegalAccessException e){
+			System.out.println("hello");
+		}
+		
+	}
+}
