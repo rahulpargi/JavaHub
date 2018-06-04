60e65498e42afbb3150621e72972b53923d6a629
Rahul Pargi <rahulpargi@gmail.com>
2018-06-04 11:07:05 -0500

    MultiThreading

--- Multi.java
+++ Multi.java
@@ -1,31 +1,46 @@
 
-class Hi{
-	public void print(){
-	for(int i=0;i<5;i++){
-			
-			System.out.println("Hi");
+class Hi extends Thread{
+	
+	public void run(){
+		for(int i=0;i<5;i++){
+			try{
+				System.out.println("Hi");
+				Thread.sleep(2000);
+			}catch(Exception e){
+
+			}	
+				
+				
 
 		}
 	}
-}
-class Hello extends Thread{
-	public void print(){
 	
+
+}
+class Hello extends Thread {
+		public void run() {
 		for(int i=0;i<5;i++){
-			
-			System.out.println("Hello");
+			try{
+				System.out.println("Hello");
+				//Thread.sleep(2000);
+			}catch(Exception e){
+
+			}	
+				
+				
 
 		}
-	}	
+	}
+	
 }
 
 class Multi extends Thread{
-	public static void main(String[] args)throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException{
 		Hello obj1=new Hello();
-		Thread.sleep(5000);
+		//Thread.sleep(5000);
 		Hi obj2=new Hi();
-		obj1.print();
-		obj2.print();
+		obj1.start();
+		obj2.start();
 
 	}
 }
