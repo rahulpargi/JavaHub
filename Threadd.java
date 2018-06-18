4ae0318fdefc3304f1c57fce6c489b3a4a9c78ac
Rahul Pargi <rahulpargi@gmail.com>
2018-06-18 08:56:29 -0500

    Thread with Runnable Interface

---
+++ Threadd.java
@@ -0,0 +1,30 @@
+class Hi implements Runnable{
+	public void run(){
+		for (int i=1;i<=5;i++){
+			System.out.println("hi");
+			try{Thread.sleep(2000);}catch(Exception e){}
+		}
+	}
+}
+class Hello implements Runnable{
+	public void run(){
+		for (int i=1;i<=5;i++){
+			System.out.println("hello");
+			try{Thread.sleep(2000);}catch(Exception e){}
+		}
+	}
+}
+
+public class Threadd{
+	public static void main(String[] args) {
+		Runnable obj1=new Hi();
+		Runnable obj2=new Hello();
+		Thread t1=new Thread(obj1);
+		Thread t2=new Thread(obj2);
+		t1.start();
+
+		try{Thread.sleep(10);}catch(Exception e){}
+		t2.start();
+	
+	}
+}
